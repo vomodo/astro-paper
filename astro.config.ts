2c439da02538b633374bdd6772ef4c28e449937f
vomodo <duc@ducvu.vn>
2025-11-03 01:59:21 +0700

    Sửa lỗi Mục lục

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     }),
   ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Mục lục" }]],
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "night-owl" },
